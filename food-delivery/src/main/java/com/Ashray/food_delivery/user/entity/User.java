106b584e334d90ceaeef3875b080501e1c3abb4d
AshrayKawalkar <17ashraykawalkar@gmail.com>
2026-06-25 12:15:24 +0530

    setup project structure and User entity

---
+++ food-delivery/src/main/java/com/Ashray/food_delivery/user/entity/User.java
@@ -0,0 +1,16 @@
+package com.Ashray.food_delivery.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+}
